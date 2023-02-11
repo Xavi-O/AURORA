23edce9fcc3ec4826e779e0cc600576dcced80e5
Xavier <odhiamboxavier@gmail.com>
2023-02-11 05:24:34 +0300

    About routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation';
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router = useRouter();
   return (
     <>
       <Head>
@@ -104,10 +106,8 @@ export default function Home() {
           </a>
 
           <a
-            href="/about"
+            onClick={() => router.push('/about')}
             className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
           >
             <h2 className={inter.className}>
               About <span>-&gt;</span>
